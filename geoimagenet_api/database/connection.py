d8a4865fd348294cc6b7603e6ac1a1fa0027899a
David Caron <dcaron05@gmail.com>
2018-12-20 17:19:36 -0500

    add configuration file and environment variables

--- geoimagenet_api/database/connection.py
+++ geoimagenet_api/database/connection.py
@@ -1,6 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from geoimagenet_api import config
 
+db = config.get("postgis_db")
+username = config.get("postgis_username")
+password = config.get("postgis_password")
 
-engine = create_engine("postgresql://postgres:postgres@192.168.99.201/gis", echo=True)
+connection_string = f"postgresql://{username}:{password}@192.168.99.201/{db}"
+
+engine = create_engine(connection_string, echo=True)
 Session = sessionmaker(bind=engine)
