ff26a2379140d0cd4c19a4bdb401e374e823f9b5
David Caron <dcaron05@gmail.com>
2018-12-21 09:54:11 -0500

    test default config

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,7 +6,7 @@ import pytest
 from geoimagenet_api import config
 
 
-@pytest.fixture(autouse=True, scope="module")
+@pytest.fixture(autouse=True)
 def force_reload_config():
     config.config.config_ini = None
 
@@ -16,13 +16,31 @@ def test_key_error():
         config.get("something_impossible")
 
 
-def test_defaults():
+@pytest.fixture
+def ignore_custom_ini(request):
+    """
+    Replace the config.ini file for the duration of the test and put it back afterwards.
+    """
+    custom_ini_path = Path(config.__file__).with_name("custom.ini")
+    previous_custom_data = None
+    if os.path.exists(custom_ini_path):
+        previous_custom_data = custom_ini_path.read_text()
+        custom_ini_path.unlink()
+
+    def write_data_back():
+        if previous_custom_data is not None:
+            custom_ini_path.write_text(previous_custom_data)
+
+    request.addfinalizer(write_data_back)
+
+
+def test_defaults(ignore_custom_ini):
     """Test that the default config is loaded"""
     db = config.get("postgis_db")
     username = config.get("postgis_username")
     password = config.get("postgis_password")
 
-    assert (db, username, password) == ('postgres', 'postgres', 'postgres')
+    assert (db, username, password) == ("postgres", "postgres", "postgres")
 
 
 @pytest.fixture
@@ -36,7 +54,9 @@ def temp_custom_ini(request):
         previous_custom_data = custom_ini_path.read_text()
     default_config_data = Path(config.__file__).with_name("default.ini").read_text()
 
-    test_config_data = re.sub(r"postgis_db = (.+)", "postgis_db = bananas", default_config_data)
+    test_config_data = re.sub(
+        r"postgis_db = (.+)", "postgis_db = bananas", default_config_data
+    )
     custom_ini_path.write_text(test_config_data)
 
     def write_data_back():
@@ -50,4 +70,6 @@ def test_custom_ini(temp_custom_ini):
     """Test that the default config is loaded"""
     db = config.get("postgis_db")
 
-    assert db == 'bananas'
+    assert db == "bananas"
+
+
