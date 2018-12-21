b9bd21eab27fa31dc71ca1d9636ff8af5f665db7
David Caron <dcaron05@gmail.com>
2018-12-21 09:49:50 -0500

    test loading custom.ini

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,14 @@
+import os
+import re
+from pathlib import Path
+
 import pytest
 from geoimagenet_api import config
+
+
+@pytest.fixture(autouse=True, scope="module")
+def force_reload_config():
+    config.config.config_ini = None
 
 
 def test_key_error():
@@ -16,4 +25,29 @@ def test_defaults():
     assert (db, username, password) == ('postgres', 'postgres', 'postgres')
 
 
+@pytest.fixture
+def temp_custom_ini(request):
+    """
+    Replace the config.ini file for the duration of the test and put it back afterwards.
+    """
+    custom_ini_path = Path(config.__file__).with_name("custom.ini")
+    previous_custom_data = None
+    if os.path.exists(custom_ini_path):
+        previous_custom_data = custom_ini_path.read_text()
+    default_config_data = Path(config.__file__).with_name("default.ini").read_text()
 
+    test_config_data = re.sub(r"postgis_db = (.+)", "postgis_db = bananas", default_config_data)
+    custom_ini_path.write_text(test_config_data)
+
+    def write_data_back():
+        if previous_custom_data is not None:
+            custom_ini_path.write_text(previous_custom_data)
+
+    request.addfinalizer(write_data_back)
+
+
+def test_custom_ini(temp_custom_ini):
+    """Test that the default config is loaded"""
+    db = config.get("postgis_db")
+
+    assert db == 'bananas'
