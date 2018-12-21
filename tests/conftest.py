4f7526f8ee75b1885be6ababd5fcaf45e5000313
David Caron <dcaron05@gmail.com>
2018-12-21 09:25:42 -0500

    update test_server config

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,14 @@ from multiprocessing import Process
 
 import pytest
 
-from geoimagenet_api import get_app
-from geoimagenet_api.tests import TEST_SERVER_PORT
+from geoimagenet_api import app
+from tests import TEST_SERVER_PORT
 
 
 @pytest.fixture(scope="module")
 def server(request):
     """Start a test server that validates both requests and responses"""
-    app = get_app(validate_responses=True)
+    app.app.validate_responses = True
     app.port = TEST_SERVER_PORT
 
     process = Process(target=app.run)
