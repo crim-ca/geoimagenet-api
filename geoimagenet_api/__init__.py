a16c6aecbb1c8bfda3ac75cc8eeda1e0c47a9b75
David Caron <dcaron05@gmail.com>
2018-12-21 10:57:14 -0500

    check database connection on startup

--- geoimagenet_api/__init__.py
+++ geoimagenet_api/__init__.py
@@ -4,6 +4,11 @@ from geoimagenet_api.__about__ import __version__, __author__, __email__
 
 import connexion
 from geoimagenet_api.utils import DataclassEncoder
+from geoimagenet_api.database import connection
+from geoimagenet_api import config
+
+if config.get('check_db_connection_on_startup'):
+    connection.check_connection()
 
 app = connexion.App(__name__, port=8080)
 app.add_api(
--- geoimagenet_api/database/connection.py
+++ geoimagenet_api/database/connection.py
@@ -5,3 +5,8 @@ from geoimagenet_api.config import get_database_url
 
 engine = create_engine(get_database_url(), echo=True)
 Session = sessionmaker(bind=engine)
+
+
+def check_connection():
+    """Checks that the connection to the database is successful"""
+    engine.table_names()
