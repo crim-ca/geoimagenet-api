4f2a021991bf733b49e050470b5be14a625b6e73
David Caron <dcaron05@gmail.com>
2018-12-21 13:57:28 -0500

    temporarily change cwd to apply migrations

--- geoimagenet_api/database/migrations.py
+++ geoimagenet_api/database/migrations.py
@@ -1,9 +1,23 @@
+from contextlib import contextmanager
+import os
+from pathlib import Path
+
 import alembic.config
 
 
+@contextmanager
+def cwd(path):
+    """Temporarily change cwd"""
+    old_pwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(old_pwd)
+
+
 def upgrade_head():
-    argv = [
-        '--raiseerr',
-        'upgrade', 'head',
-    ]
-    alembic.config.main(argv=argv)
+    here = str(Path(__file__).parent)
+    with cwd(here):
+        argv = ["--raiseerr", "upgrade", "head"]
+        alembic.config.main(argv=argv)
