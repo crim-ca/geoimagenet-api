7a45966ad4acbaf517dcb683027352daa5968b4f
David Caron <dcaron05@gmail.com>
2019-01-07 13:23:37 -0500

    reset the database to a clean state before running tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,39 @@
+import os
+
 import pytest
 
+from geoimagenet_api.database import migrations
 from geoimagenet_api import app
+from geoimagenet_api.database.connection import get_engine
+
+
+def reset_test_database():
+    """Reset the database to a brand new clean state.
+
+    - Create the database if it doesn't exist and install postgis extension
+    - Drop all tables except 'spatial_ref_sys'
+    - Initialize the database with data (taxonomy, etc.)
+    """
+    migrations.ensure_database_exists()
+
+    engine = get_engine()
+    for table in engine.table_names():
+        if not table == "spatial_ref_sys":
+            engine.execute(f"DROP TABLE IF EXISTS {table} CASCADE;")
+
+    # run migrations and initiate base data
+    migrations.init_database_data()
 
 
 @pytest.fixture(scope="module")
 def client():
     app.app.validate_responses = True
+
+    # configuration
+    os.environ["GEOIMAGENET_API_POSTGIS_DB"] = "geoimagenet_test"
+    os.environ["GEOIMAGENET_API_VERBOSE_SQLALCHEMY"] = "false"
+
+    reset_test_database()
+
     with app.app.test_client() as c:
         yield c
