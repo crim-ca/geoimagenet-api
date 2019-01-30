adf4763a006238733812a7567e6632c90487c0d1
David Caron <dcaron05@gmail.com>
2019-01-30 14:57:04 -0500

    chg: dev: reformatting

--- geoimagenet_api/routes/annotations.py
+++ geoimagenet_api/routes/annotations.py
@@ -1,5 +1,4 @@
 import json
-from typing import Dict
 
 from flask import request
 from sqlalchemy.exc import IntegrityError
--- tests/locustfile.py
+++ tests/locustfile.py
@@ -10,28 +10,45 @@ def taxo(l):
 
 
 def classes(l):
-    l.client.get("/taxonomy_classes/1")
+    l.client.get("/taxonomy_classes/1", params={"depth": 10}, verify=False)
 
 
 def add_annotation_geojson(l):
-    data = {"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [
-        [[-13621076.051761277, 6281028.872735048], [-13621052.165189937, 6280804.33896446],
-         [-13620884.959190564, 6280923.771821156], [-13621076.051761277, 6281028.872735048]]]},
-     "properties": {"taxonomy_class_id": 8, "annotator_id": 1, "image_name": "My Image"}}
+    data = {
+        "type": "Feature",
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [-13621076.051761277, 6281028.872735048],
+                    [-13621052.165189937, 6280804.33896446],
+                    [-13620884.959190564, 6280923.771821156],
+                    [-13621076.051761277, 6281028.872735048],
+                ]
+            ],
+        },
+        "properties": {
+            "taxonomy_class_id": 8,
+            "annotator_id": 1,
+            "image_name": "My Image",
+        },
+    }
     l.client.post("/annotations", json=data, verify=False)
 
 
 def add_annotation_xml(l):
     data = '<Transaction xmlns="http://www.opengis.net/wfs" service="WFS" version="1.1.0" xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><Insert><annotation xmlns="geoimagenet"><geometry><Polygon xmlns="http://www.opengis.net/gml" srsName="EPSG:3857"><exterior><LinearRing srsName="EPSG:3857"><posList srsDimension="2">-13624202.803949567 6279980.252253259 -13624432.115034422 6279330.537512835 -13623725.072522784 6279483.411569405 -13624202.803949567 6279980.252253259</posList></LinearRing></exterior></Polygon></geometry><taxonomy_class_id>8</taxonomy_class_id><annotator_id>1</annotator_id><image_name>My Image</image_name></annotation></Insert></Transaction>'
-    l.client.post("/geoserver/wfs", data=data, headers={'Content-Type': 'text/xml'}, verify=False)
+    l.client.post(
+        "/geoserver/wfs", data=data, headers={"Content-Type": "text/xml"}, verify=False
+    )
 
 
 class UserBehavior(TaskSet):
     tasks = {
         # index: 1,
-        add_annotation_geojson: 1,
+        # add_annotation_geojson: 1,
         # add_annotation_xml: 1,
-        # classes: 1,
+        classes: 1
     }
 
 
@@ -39,16 +56,3 @@ class WebsiteUser(HttpLocust):
     task_set = UserBehavior
     min_wait = 700
     max_wait = 1300
-
-if __name__ == '__main__':
-    import requests
-    import json
-
-    data = {"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [
-        [[-13621076.051761277, 6281028.872735048], [-13621052.165189937, 6280804.33896446],
-         [-13620884.959190564, 6280923.771821156], [-13621076.051761277, 6281028.872735048]]]},
-            "properties": {"taxonomy_class_id": 8, "annotator_id": 1, "image_name": "My Image"}}
-    r = requests.post("https://192.168.99.201/api/v1/annotations",
-                  json=data,
-                  verify=False)
-    print(r.json())
