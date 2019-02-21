128a535d20f409a4924db723d0c0801ba4572a1f
David Caron <dcaron05@gmail.com>
2019-02-20 21:36:09 -0500

    chg: dev: implement GET on /batches/{taxonomy_id} and stream large data

--- geoimagenet_api/routes/batches.py
+++ geoimagenet_api/routes/batches.py
@@ -1,8 +1,51 @@
+import json
+
 from sqlalchemy import func, cast
 from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy import and_
 
+from flask import Response
+
 from geoimagenet_api.routes.taxonomy_classes import get_all_taxonomy_classes_ids
-from geoimagenet_api.database.models import Annotation as DBAnnotation
+from geoimagenet_api.database.models import Annotation as DBAnnotation, AnnotationStatus
 from geoimagenet_api.database.connection import connection_manager
 
+
+def get_annotations(taxonomy_id):
+    with connection_manager.get_db_session() as session:
+        taxonomy_ids = get_all_taxonomy_classes_ids(session, taxonomy_id)
+
+        query = session.query(
+            func.json_build_object(
+                "type",
+                "Feature",
+                "geometry",
+                cast(func.ST_AsGeoJSON(DBAnnotation.geometry), JSON),
+                "properties",
+                func.json_build_object(
+                    "image_name",
+                    DBAnnotation.image_name,
+                    "taxonomy_class_id",
+                    DBAnnotation.taxonomy_class_id,
+                ),
+            )
+        ).filter(
+            and_(
+                DBAnnotation.status == AnnotationStatus.validated,
+                DBAnnotation.taxonomy_class_id.in_(taxonomy_ids),
+            )
+        )
+
+        # try to stream the geojson features from the database
+        # so that the whole FeatureCollection is not built entirely in memory
+
+        def geojson_stream():
+            yield '{"type": "FeatureCollection", "features": ['
+            n_features = query.count()
+            for n, r in enumerate(query):
+                feature = json.dumps(r[0])
+                if n != n_features - 1:
+                    feature += ','
+                yield feature
+            yield ']}'
+        return Response(geojson_stream(), mimetype='application/json')
---
+++ tests/test_batches.py
@@ -0,0 +1,42 @@
+import pp
+
+from geoimagenet_api.database.connection import connection_manager
+from geoimagenet_api.database.models import Annotation
+from tests.utils import api_url
+
+
+def test_get_annotations(client):
+    # ----- given
+    with connection_manager.get_db_session() as session:
+        some_annotations = []
+        for _ in range(3):
+            some_annotations.append(
+                Annotation(
+                    annotator_id=1,
+                    geometry="SRID=3857;POLYGON((0 0,1 0,1 1,0 1,0 0))",
+                    taxonomy_class_id=2,
+                    image_name="my image",
+                    status="validated"
+                )
+            )
+            session.add(some_annotations[-1])
+        session.commit()
+        some_annotations_ids = [a.id for a in some_annotations]
+
+    # ----- when
+    query = {'taxonomy_id': 1}
+    r = client.get(api_url("/batches"), query_string=query)
+
+    # ----- then
+    assert r.status_code == 200
+    assert len(r.json['features']) == 3
+    first_feature = r.json['features'][0]
+    assert 'image_name' in first_feature['properties']
+    assert 'taxonomy_class_id' in first_feature['properties']
+
+    # ----- cleanup
+    with connection_manager.get_db_session() as session:
+        session.query(Annotation).filter(
+            Annotation.id.in_(some_annotations_ids)
+        ).delete(synchronize_session=False)
+        session.commit()
