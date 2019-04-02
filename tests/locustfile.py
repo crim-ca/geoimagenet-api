964f25607a91f5809ba3c32e32a261265090bd08
David Caron <dcaron05@gmail.com>
2019-04-02 16:01:12 -0400

    chg: dev: update locustfile

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -6,19 +6,19 @@ def index(l):
 
 
 def taxo(l):
-    l.client.get("/taxonomy/objets/1")
+    l.client.get("/taxonomy/objets/1", verify=False)
 
 
 def classes(l):
-    l.client.get("/taxonomy_classes/1", params={"depth": 10}, verify=False)
+    l.client.get("/taxonomy_classes/1", verify=False)
 
 
 def classes_search(l):
-    l.client.get("/taxonomy_classes", params={"taxonomy_name": "Objets", "id": 1, "depth": 10}, verify=False)
+    l.client.get("/taxonomy_classes", params={"taxonomy_name": "Objets", "id": 1}, verify=False)
 
 
 def classes_depth_0(l):
-    l.client.get("/taxonomy_classes/1", params={"depth": 0}, verify=False)
+    l.client.get("/taxonomy_classes/1", verify=False)
 
 
 def add_annotation_geojson(l):
@@ -30,7 +30,6 @@ def add_annotation_geojson(l):
                 [
                     [-13621076.051761277, 6281028.872735048],
                     [-13621052.165189937, 6280804.33896446],
-                    [-13620884.959190564, 6280923.771821156],
                     [-13621076.051761277, 6281028.872735048],
                 ]
             ],
@@ -53,10 +52,11 @@ def add_annotation_xml(l):
 
 class UserBehavior(TaskSet):
     tasks = {
-        # index: 1,
+        taxo: 1,
         # add_annotation_geojson: 1,
         # add_annotation_xml: 1,
-        classes: 1,
+        # taxo: 1,
+        # classes: 1,
         # classes_search: 1,
         # classes_depth_0: 1
     }
