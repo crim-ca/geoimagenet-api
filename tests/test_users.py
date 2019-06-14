b8a883186478741c7c38b18996eb96cdfa49a0ba
David Caron <dcaron05@gmail.com>
2019-06-14 15:23:40 -0400

    chg: dev: remove unnecessary test

--- tests/test_users.py
+++
@@ -1,7 +0,0 @@
-def test_get_users(client):
-    assert client.get('/users/observateur').json()['email'] == "observateur@crim.ca"
-    assert client.get('/users/annotateur').json()['email'] == "annotateur@crim.ca"
-    assert client.get('/users/validateur').json()['email'] == "validateur@crim.ca"
-    assert client.get('/users/admin').json()['email'] == "admin@crim.ca"
-
-    assert client.get('/users/qqqqq').status_code == 404
