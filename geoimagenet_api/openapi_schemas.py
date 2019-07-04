eb2640478641edc01c0dff97e6942f7bde4c2417
David Caron <dcaron05@gmail.com>
2019-07-04 14:40:32 -0400

    chg: dev: fix mysterious bug in fastapi or pydantic

--- geoimagenet_api/openapi_schemas.py
+++ geoimagenet_api/openapi_schemas.py
@@ -76,7 +76,8 @@ class ExecuteIOHref(BaseModel):
 
 class BatchPostForwarded(BaseModel):
     inputs: List[Union[ExecuteIOValue, ExecuteIOHref]]
-    outputs: List[Union[ExecuteIOValue, ExecuteIOHref]] = []
+    # Todo: replace Any with ExecuteIOValue when fastapi and pydantic are updated and this mysterious bug is fixed
+    outputs: List[Union[Any, ExecuteIOHref]] = []
 
 
 class BatchPostResult(BaseModel):
